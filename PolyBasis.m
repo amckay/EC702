de06c4053ea41b0b6532be0ac6a7fe7dd7485733
Alisdair McKay <alisdair.mckay@gmail.com>
2016-08-06 20:40:47 -0400

    Cubic interpolant

--- PolyBasis.m
+++ PolyBasis.m
@@ -1,18 +1,15 @@
-function B  = PolyBasis(Grid)
+function B  = PolyBasis(K,Z)
 % B  = PolyBasis(K,Z)
 % Polynomial basis functions.  Using 2nd order polynomial
 %
 % inputs
-% Grid.KZ has two columsn
 % K    n x 1   points for K
 % Z    n x 1   points for Z
 %
 % outputs
 % B    n x 6   array of basis functions: 1, K, Z, K^2, K*Z, Z^2
 
-K = Grid.KZ(:,1);
-Z = Grid.KZ(:,2);
-B = [ones(size(K)) K Z K.^2 K.*Z Z.^2];
+B = [ones(size(K)) K Z K.^2 K.*Z Z.^2 K.^3 K.^2.*Z K.*Z.^2 Z.^3];
 
 
 end
--- VFI.m
+++ VFI.m
@@ -42,19 +42,17 @@ Grid.PZ = Grid.PZ'; % this is a 7 x 7 transition matrix for which the columns su
 
 %% Create a grid for K
 Grid.nK = 20;
-Grid.K = linspace(SteadyState(Par,Grid.Z(1)), SteadyState(Par,Grid.Z(end)),Grid.nK)';  % this is a 20 x 1 array
+Grid.K = linspace(0.9*Kstar, 1.1*Kstar,Grid.nK)';  % this is a 20 x 1 array
 
 %% Create a product of the two grids, the basis functions, and invert them
 Grid.KZ = [kron(ones(Grid.nZ,1),Grid.K)  kron(Grid.Z,ones(Grid.nK,1))];
-Grid.BasisFuncs = PolyBasis(Grid);
-Grid.BasisFuncsInv = inv(Grid.BasisFuncs' * Grid.BasisFuncs);
 
 
 %% Initial guess of value function -> all zeros
 
 %Note: we will approximate E[V(K',Z') | Z] so the unknown function is a
 %function of K' and Z.
-b0 = zeros(6,1);
+b0 = zeros(10,1);
 
 
 %% Bellman iteration
@@ -62,8 +60,35 @@ Kp = zeros(Grid.nK,Grid.nZ);  % Array to hold the savings decisions.
 
 u = @(C) C.^(1-Par.gamma)/(1-Par.gamma);
 f = @(K,Z) exp(Z) .* K.^Par.alpha + (1-Par.delta)*K;
-Bellman = @(K,Z,Kp,b) u(f(K,Z) - Kp) + Par.beta * PolyBasis(Kp,Z) * b;
+Bellman = @(Kp,K,Z,b) u( f(K,Z) - Kp) ...
+                     + Par.beta * PolyBasis(Kp,Z) * b;
+NegBellman = @(Kp,K,Z,b) -Bellman(Kp,K,Z,b);
 
-% for iK = 1:Grid.nK
-%     for iZ = 1:Grid.nZ
-%         Kp(iK,iZ) = fminbnd(
+for it = 1:1000
+    for iK = 1:Grid.nK
+        for iZ = 1:Grid.nZ
+            Kp(iK,iZ) = fminbnd(NegBellman,Grid.K(1),Grid.K(2),optimset('TolX',1e-12),...
+                Grid.K(iK),Grid.Z(iZ),b0);
+        end
+    end
+
+    % evaluate the Bellman equation at the optimal policy to find the new
+    % value function.
+    V = Bellman(Kp(:),Grid.KZ(:,1),Grid.KZ(:,2),b0);
+    
+    % take the expectation of the value function from the perspective of
+    % the previous Z
+    EV = reshape(V,Grid.nK,Grid.nZ) * Grid.PZ; 
+    
+    % update our polynomial coefficients
+    b1 = PolyGetCoef(Grid.KZ(:,1),Grid.KZ(:,2),EV(:));
+    
+    % see how much our coefficients have changed
+    test = max(abs(b1 - b0));
+    
+    disp(['iteration ' num2str(it) ', test = ' num2str(test)])
+    if test < 1e-5
+        break
+    end
+    b0 = b1;
+end
