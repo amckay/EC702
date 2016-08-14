bc9243211103df19384bde9bf53ed132082a45f8
Alisdair McKay <alisdair.mckay@gmail.com>
2016-08-14 14:51:44 -0400

    Remove old VFI

--- code/VFI.m
+++ code/VFI.m
@@ -23,63 +23,46 @@ setup;
 
 %Note: we will approximate E[V(K',Z') | Z] so the unknown function is a
 %function of K' and Z.
-b0 = zeros(6,1);
+b = zeros(6,1);
 
 
 %% Bellman iteration
-Kp = zeros(Grid.nK,Grid.nZ);  % Array to hold the savings decisions.
 
-u = @(C) C.^(1-Par.gamma)/(1-Par.gamma);
+Kp0 = zeros(size(Grid.KK));
+MAXIT = 2000;
+for it = 1:MAXIT
 
-Bellman = @(Kp,K,Z,b) u( f(Par,K,Z) - Kp) ...
-                     + Par.beta * PolyBasis(Kp,Z) * b;
-NegBellman = @(Kp,K,Z,b) -Bellman(Kp,K,Z,b);
+    [V, Kp] = MaxBellman(Par,b,Grid);
 
-for it = 1:1000
-    for iK = 1:Grid.nK
-        for iZ = 1:Grid.nZ
-            % first find the point at which consumption is negative
-            % Kp = f(K);
-            MaxKp = f(Par,Grid.K(iK),Grid.Z(iZ)) - 1e-3; % -1e-3 so we always have positve consumption.
-            
-            Kp(iK,iZ) = fminbnd(NegBellman,Grid.K(1),MaxKp,optimset('TolX',1e-12),...
-                Grid.K(iK),Grid.Z(iZ),b0);
-        end
-    end
-
-    % evaluate the Bellman equation at the optimal policy to find the new
-    % value function.
-    V = Bellman(Kp(:),Grid.KK,Grid.ZZ,b0);
     
     % take the expectation of the value function from the perspective of
     % the previous Z
     EV = reshape(V,Grid.nK,Grid.nZ) * Grid.PZ; 
     
     % update our polynomial coefficients
-    b1 = PolyGetCoef(Grid.KK,Grid.ZZ,EV(:));
+    b = PolyGetCoef(Grid.KK,Grid.ZZ,EV(:));
     
     % see how much our coefficients have changed
-    test = max(abs(b1 - b0));
-    
+    test = max(abs(Kp0 - Kp));
+    Kp0 = Kp;
     disp(['iteration ' num2str(it) ', test = ' num2str(test)])
     if test < 1e-5
         break
     end
-    b0 = b1;
 end
 
+%% Make plot of policy rule
+
+DK = Grid.K/Kstar-1;
+DKp = reshape(Kp,Grid.nK,Grid.nZ)./reshape(Grid.KK,Grid.nK,Grid.nZ) - 1;
+plot(DK, DKp);
+hold on;
+plot(DK, zeros(Grid.nK,1), 'k--');
+xlabel('K in % deviation from steady state')
+ylabel('(K'' - K)/K')
 
 %%  Consumption and savings at K = 29, Z = 0.03
-% we need to get C (or Kp)
-% we know V
-% from the Envelope condition we have V_K = u'(C) f_K(K,Z)
-% from this we can do C = (V_K / f_K)^(-1/Par.gamma)
-% how do we get V_K?
-% V = sum_i b_i Fi(K,Z) where Fi are the basis functions
-% so 
-% V_K = sum_i b_i Fi_K(K,Z)
 
-FIXME: wrong, we did not approx V but E(V)
-V_K = PolyBasisDeriv(29,0.03) * b0;
-C = (V_K / fprime(Par,29,0.03))^(-1/Par.gamma)
-Kp = f(Par,29,0.03) - C
+bKp =  PolyGetCoef(Grid.KK,Grid.ZZ,Kp);
+Kp2903 = PolyBasis(29,0.03) * bKp
+C2903 = f(Par,29,0.03) - Kp2903
